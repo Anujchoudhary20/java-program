da973a5e7e756e35b9c3223a39ba7c32723e706d
Anujchoudhary20 <165000310+Anujchoudhary20@users.noreply.github.com>
2024-03-26 21:39:18 +0530

    Add files via upload

---
+++ Multistar.java
@@ -0,0 +1,31 @@
+package javaclass;
+
+public class Multistar {
+	public static void main(String[] args) {
+		System.out.println("star partel in java");
+		for(int i=0;i<9;i++) {
+			for(int j=1;j<=9;j++) {
+				System.out.print(i );
+			}
+			System.out.println();	
+		}
+		
+		
+		for(int i=9;i>0;i--) {
+			for(int j=1;j<=9;j++) {
+				System.out.print(i);
+			}
+			System.out.println();	
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
