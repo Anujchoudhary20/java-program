debeeec9b576e0aa9e4c66531f02edabb817deeb
Anujchoudhary20 <165000310+Anujchoudhary20@users.noreply.github.com>
2024-03-26 21:36:38 +0530

    Add files via upload

---
+++ firstjavaclass.java
@@ -0,0 +1,11 @@
+
+public class firstjavaclass {
+
+    public static void main(String[] args) {
+        System.out.println("anuj choudhary");
+        System.out.println("anuj choudhary");
+        System.out.println("anuj choudhary");
+       
+    }
+
+}
